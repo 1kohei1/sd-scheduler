c2456360f9141c0c5c8dcf74fc6c6feda409278a
Kohei Arai <tobecomebig@gmail.com>
2017-12-29 16:14:02 +0900

    Updated index.js to list batman tv series

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,36 @@
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
 import Layout from '../components/Layout';
 
-const PostLink = (props) => (
+const ShowLink = (props) => (
   <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{ props.title }</a>
+    <Link as={`/p/${props.show.id}`} href={`/post?id=${props.show.id}`}>
+      <a>{ props.show.name }</a>
     </Link>
   </li>
 )
 
-const Index = () => (
+const Index = (props) => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      {props.shows.map(({show}) => (
+        <ShowLink show={show} key={show.id}></ShowLink>
+      ))}
     </ul>
   </Layout>
 )
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  }
+}
 
 export default Index
