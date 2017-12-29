f8926592fa0a5817efb8df8fc638e623d310a228
Kohei Arai <tobecomebig@gmail.com>
2017-12-29 15:46:32 +0900

    Updated index.js to use client side route masking

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 
 const PostLink = (props) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{ props.title }</a>
     </Link>
   </li>
@@ -14,9 +14,9 @@ const Index = () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Next.js is awesome" />
-      <PostLink title="Deploy apps with Zeit" />
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
     </ul>
   </Layout>
 )
