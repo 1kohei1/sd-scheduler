9c9a50a1acc3377d3070603d03e8db78d7be45ee
Kohei Arai <tobecomebig@gmail.com>
2017-12-29 13:12:53 +0900

    Updated index.js to have blog posts

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,22 @@ import Link from 'next/link';
 
 import Layout from '../components/Layout';
 
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post/${props.title}`}>
+      <a>{ props.title }</a>
+    </Link>
+  </li>
+)
+
 const Index = () => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
   </Layout>
 )
 
