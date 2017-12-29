ba374191eccdf486fa687745b692c8eb93ff8811
Kohei Arai <tobecomebig@gmail.com>
2017-12-29 12:39:39 +0900

    Updated index.js to include the query

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Link from 'next/link';
 
 const Index = () => (
   <div>
-    <Link href="/about">
+    <Link href={{ pathname: '/about', query: {name: 'abc' }}}>
       <a>About page</a>
+    </Link>
+    <Link href={{ pathname: '/about', query: {name: 'abc' }}}>
+      <button>Go to about page</button>
     </Link>
     <p>Hello Next.js</p>
   </div>
