433024336b933f5e53e94433993efd496e4a3674
Kohei Arai <tobecomebig@gmail.com>
2018-01-02 12:33:47 +0900

    Fixed index.tsx typescript error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,49 @@
+import * as React from 'react';
 import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
 import { List } from 'antd';
+const fetch = require('isomorphic-unfetch');
 
 import MyLayout from '../components/MyLayout';
 
-const Index = (props) => (
-  <MyLayout>
-    <h1>Batman TV Shows</h1>
-    <List 
-      size="small"
-      bordered
-      dataSource={props.shows}
-      renderItem={({show}) => (
-        <List.Item>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{ show.name }</a>
-          </Link>
-        </List.Item>
-      )}
-    />
-  </MyLayout>
-)
+interface Show {
+  id: string;
+  name: string;
+}
 
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
+interface Props {
+  shows: Array<Show>;
+}
 
-  console.log(`Show data fetched. Count: ${data.length}`);
+class Index extends React.Component<Props, {}> {
+  static async getInitialProps() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+  
+    console.log(`Show data fetched. Count: ${data.length}`);
+  
+    return {
+      shows: data
+    }
+  }
 
-  return {
-    shows: data
+  render() {
+    return (
+      <MyLayout>
+        <h1>Batman TV Shows</h1>
+        <List 
+          size="small"
+          bordered
+          dataSource={this.props.shows}
+          renderItem={({show}: {show: Show}) => (
+            <List.Item>
+              <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                <a>{ show.name }</a>
+              </Link>
+            </List.Item>
+          )}
+        />
+      </MyLayout>
+    )
   }
 }
 
