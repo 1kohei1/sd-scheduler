5f365990c7bd627788d573a5c2f367c662300a24
Kohei Arai <tobecomebig@gmail.com>
2018-01-05 00:19:17 +0900

    Created pages/users/index.ts

---
+++ pages/users/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import MyLayout from '../../components/MyLayout';
+
+class UserIndex extends React.Component {
+  render() {
+    return (
+      <MyLayout>
+        <h1>Hello Users</h1>
+      </MyLayout>
+    )
+  }
+}
+
+export default UserIndex;
