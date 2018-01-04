4b0aa2b15f57d24ba0b71dd8c4df50067fbbc85b
Kohei Arai <tobecomebig@gmail.com>
2018-01-04 18:42:12 +0900

    Added InitialProps.tsx and post.tsx

---
+++ pages/post.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Link from 'next/link';
+const fetch = require('isomorphic-unfetch');
+
+import MyLayout from '../components/MyLayout'
+import Show from '../models/Show';
+import InitialProps from '../models/InitialProps';
+
+export interface PostProps {
+  show: Show;
+}
+
+class Post extends React.Component<PostProps, any> {
+  static async getInitialProps(context:InitialProps) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+  
+    console.log(`Fetched show: ${show.name}`)
+  
+    return { show };
+  }
+
+  render() {
+    return (
+      <MyLayout>
+        <h1>{ this.props.show.name }</h1>
+        <p>{ this.props.show.summary.replace(/<[/]?p>/g, '') }</p>
+        <img src={ this.props.show.image.medium } alt="Medium image"/>
+      </MyLayout>
+    );
+  }
+}
+
+export default Post;
