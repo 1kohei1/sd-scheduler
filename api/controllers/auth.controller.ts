f0a0869776199691f1ea7b4661bc32694487f189
Kohei Arai <tobecomebig@gmail.com>
2018-01-12 21:34:38 +0900

    Created auth routes and controller

---
+++ api/controllers/auth.controller.ts
@@ -0,0 +1,2 @@
+import { Request, Response } from 'express';
+
--- api/routes/auth.route.ts
+++ api/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Application, Request, Response } from 'express';
+const authController = require('../controllers/auth.controller');
 
 module.exports = (server: Application) => {
-  require('./sample.route')(server);
-  require('./auth.route')(server);
+  server.get('/api/auth/login', );
 }
