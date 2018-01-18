74aa4c978e115d63d63565400f4f60461689a0e4
Kohei Arai <tobecomebig@gmail.com>
2018-01-18 20:44:00 +0900

    Updated Location to toggle information and form

--- front/components/Location.tsx
+++ front/components/Location.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import * as shortid from 'shortid';
-import { Card, Button } from 'antd';
+import { Form, Icon, Input, Card, Button } from 'antd';
+import { WrappedFormUtils } from 'antd/lib/form/Form';
 
 import { Semester } from '../models/Semester';
 
 export interface LocationProps {
+  form: WrappedFormUtils,
   semester: Semester,
   editing: boolean;
   toggleForm: (menu: string) => void;
   updateSemester: (updateObj: any, menu: string) => void;
 }
 
-export default class Location extends React.Component<LocationProps, any> {
+class Location extends React.Component<LocationProps, {}> {
   constructor(props: LocationProps) {
     super(props);
 
@@ -32,7 +34,21 @@ export default class Location extends React.Component<LocationProps, any> {
 
   form() {
     return (
-      <div>Hello</div>
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Item>
+          {this.props.form.getFieldDecorator('location')(
+            <Input prefix={<Icon type="home" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Presentation location" />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit" type="primary" style={{ marginRight: '16px' }}>
+            Update
+          </Button>
+          <Button onClick={(e) => this.props.toggleForm('location')}>
+            Cancel
+          </Button>
+        </Form.Item>
+      </Form>
     )
 
   }
@@ -41,20 +57,31 @@ export default class Location extends React.Component<LocationProps, any> {
     const isAdmin = true;
     const isArchived = false;
 
-    const extra = isAdmin && !isArchived ? (
-      <Button ghost
+    let extra: string | JSX.Element = '';
+    if (isAdmin && !isArchived && this.props.editing) {
+      extra = (<Button 
+        icon="close"
+        size="small"
+        onClick={(e) => this.props.toggleForm('location')}
+      >
+        Cancel
+      </Button>)
+    } else if (isAdmin && !isArchived) {
+      extra = (<Button ghost
         type="primary"
         size="small"
         onClick={(e) => this.props.toggleForm('location')}
       >
         Edit location
-      </Button>
-    ) : (``);
+      </Button>);
+    }
 
     return (
       <Card title="Location" extra={extra} style={{ marginBottom: '16px' }}>
-        {this.props.editing ? this.form() : this.info() }
+        {this.props.editing ? this.form() : this.info()}
       </Card>
     );
   }
 }
+
+export default Form.create()(Location);
