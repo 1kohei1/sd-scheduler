83e052625062fb6622f16a3a49dfcd5bb52cf394
Kohei Arai <tobecomebig@gmail.com>
2018-01-28 13:49:59 +0900

    Updated Presentation interface to omit id from the property name

--- front/models/Presentation.ts
+++ front/models/Presentation.ts
@@ -4,8 +4,8 @@ export default interface Presentation {
   _id: string;
   start: Moment;
   end: Moment;
-  semesterId: string;
-  groupId: string; // This will be replaced with actual group interface, but let's keep it as string id
+  semester: string;
+  group: string; // This will be replaced with actual group interface, but let's keep it as string id
   faculties: any[]; // This will be replaced with actual faculty interface, but let's keep it like this
   midPresentationLink: string;
   committeeFormLink: string;
