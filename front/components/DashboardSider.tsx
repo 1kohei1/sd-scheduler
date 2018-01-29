156d17947edb957b8772f8e050600818ed93c19a
Kohei Arai <tobecomebig@gmail.com>
2018-01-30 00:17:18 +0900

    Updated DashboardSider to make the menu openable in new tab

--- front/components/DashboardSider.tsx
+++ front/components/DashboardSider.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Router from 'next/router';
 import { Menu } from 'antd';
+import Link from 'next/link'
 
 import { Semester, Menus } from '../models/Semester';
 import { ClickParam } from 'antd/lib/menu';
@@ -15,47 +16,35 @@ export interface DashboardSiderProps {
 export default class DashboardSider extends React.Component<DashboardSiderProps, any> {
   constructor(props: DashboardSiderProps) {
     super(props);
-    this.onMenuItemClick = this.onMenuItemClick.bind(this);
-  }
-
-  onMenuItemClick({key}: ClickParam) {
-    let year: string, season: string, menu: string;
-    [year, season, menu] = key.split("_");
-    const semester = `${year}_${season}`;
-    const openKeys = this.props.openKeys.join(',');
-
-    Router.push({
-      pathname: '/dashboard',
-      query: {
-        semester,
-        menu,
-        openKeys
-      }
-    }, `/dashboard/${year}_${season}/${menu}`);
   }
 
   render() {
     return (
-      <Menu 
+      <Menu
         style={{ width: '200px', minHeight: "calc(100vh - 64px)", height: '100%' }}
         openKeys={this.props.openKeys}
         selectedKeys={this.props.selectedKeys}
-        onClick={this.onMenuItemClick}
         mode="inline"
       >
-        { this.props.semesters.map(semester => (
-          <Menu.SubMenu 
+        {this.props.semesters.map(semester => (
+          <Menu.SubMenu
             key={semester.key}
             title={semester.displayName}
             onTitleClick={this.props.onSubMenuTitleClick}
           >
-            { Menus.map(menu => (
+            {Menus.map(menu => (
               <Menu.Item key={`${semester.key}_${menu.key}`}>
-                {menu.displayName}
+                <Link
+                  as={`/dashboard/${semester.key}/${menu.key}`}
+                  href={`/dashboard?semester=${semester.key}&menu=${menu.key}&openKeys=${this.props.openKeys.join(',')}`}
+                >
+                  <a>{menu.displayName}</a>
+                </Link>
               </Menu.Item>
-            )) }
+
+            ))}
           </Menu.SubMenu>
-        )) }
+        ))}
 
       </Menu>
     );
