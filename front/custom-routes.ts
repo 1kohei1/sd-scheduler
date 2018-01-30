08e06c4cb16373a20f372c84dda2ec8d650f0ed8
Kohei Arai <tobecomebig@gmail.com>
2018-01-30 17:18:42 +0900

    Updated index page to display the filtering for student's calendar

--- front/custom-routes.ts
+++ front/custom-routes.ts
@@ -2,12 +2,6 @@ import { Server } from 'next';
 import { Application, Request, Response } from 'express';
 
 module.exports = (app: Server, server: Application) => {
-  server.get('/p/:id', (req: Request, res: Response) => {
-    const actualPage = '/post';
-    const queryParams = {id: req.params.id};
-    app.render(req, res, actualPage, queryParams);
-  });
-
   server.get('/users/:id', (req: Request, res: Response) => {
     const actualPage = '/users';
     const queryParams = { id: req.params.id };
