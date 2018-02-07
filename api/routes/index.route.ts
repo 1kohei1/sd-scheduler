761e6b030da654bca1a6080543c05c8cfaba7b75
Kohei Arai <tobecomebig@gmail.com>
2018-02-07 10:20:36 +0900

    Refactored index.route.ts to load all files under the routes folder

--- api/routes/index.route.ts
+++ api/routes/index.route.ts
@@ -1,8 +1,17 @@
 import { Application, Request, Response } from 'express';
 
 module.exports = (server: Application) => {
-  require('./sample.route')(server);
-  require('./user.route')(server);
-  require('./semester.route')(server);
-  require('./faculty.route')(server);
+  const normalizedPath = require("path").join(__dirname);
+
+  const files: string[] = [];
+  require("fs").readdirSync(normalizedPath).forEach(function (file: string) {
+    const arr = file.split('.');
+    const fileName = `./${arr[0]}.route`;
+
+    if (!files.includes(fileName) && !fileName.includes('index')) {
+      console.log(`Loading ${fileName}`);
+      require(fileName)(server);
+      files.push(fileName);
+    }
+  });
 }
