8ae8e1ea744f10b3750f7a05babaf4884df10649
Kohei Arai <tobecomebig@gmail.com>
2018-02-07 09:39:34 +0900

    Created AvailableSlot schema definition

---
+++ api/models/AvailableSlot.model.ts
@@ -0,0 +1,31 @@
+import { Model, model, Schema } from 'mongoose';
+
+const AvailableSlotSchema = new Schema({
+  faculty: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Faculty',
+  },
+  semester: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Faculty',
+  },
+  availableSlots: [{
+    start: Date,
+    end: Date,
+  }],
+  created_at: Date,
+  updated_at: Date,
+});
+
+AvailableSlotSchema.pre('save', function(this: any, next) {
+  if (!this.created_at) {
+    this.created_at = new Date();
+  }
+  this.updated_at = new Date();
+
+  next();
+});
+
+export default model('AvailableSlot', AvailableSlotSchema);
