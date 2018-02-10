b8b73caa78595fd603f6c888a047b04af91c9cc9
Kohei Arai <tobecomebig@gmail.com>
2018-02-11 03:09:14 +0900

    Deleted unused files

--- front/custom-routes.ts
+++ front/custom-routes.ts
@@ -4,12 +4,6 @@ import { Application, Request, Response } from 'express';
 import APIUtil from '../api/utils/api.util';
 
 module.exports = (app: Server, server: Application) => {
-  server.get('/users/:id', (req: Request, res: Response) => {
-    const actualPage = '/users';
-    const queryParams = { id: req.params.id };
-    app.render(req, res, actualPage, queryParams);
-  });
-
   server.get(
     '/password/:token',
     (req: Request, res: Response) => {
@@ -19,27 +13,29 @@ module.exports = (app: Server, server: Application) => {
     }
   )
 
-  server.get('/dashboard/*', (req: Request, res: Response) => {
-    let url = req.url;
-    // Remove the first slash and the last slash
-    if (url.charAt(0) === '/') {
-      url = url.substring(1);
-    }
-    if (url.charAt(url.length - 1) === '/') {
-      url = url.substring(0, url.length - 1);
-    }
-    // Convert to query. Validation for the value happens on the client side
-    // Remove the first element since it is always dashboard
-    const query = url.split('/').slice(1) || [];
-    let queryParams:any = { };
-    if (query[0]) {
-      queryParams.semester = query[0];
-    }
-    if (query[1]) {
-      queryParams.menu = query[1];
-    }
+  server.get(
+    '/dashboard/*',
+    (req: Request, res: Response) => {
+      let url = req.url;
+      // Remove the first slash and the last slash
+      if (url.charAt(0) === '/') {
+        url = url.substring(1);
+      }
+      if (url.charAt(url.length - 1) === '/') {
+        url = url.substring(0, url.length - 1);
+      }
+      // Convert to query. Validation for the value happens on the client side
+      // Remove the first element since it is always dashboard
+      const query = url.split('/').slice(1) || [];
+      let queryParams: any = {};
+      if (query[0]) {
+        queryParams.semester = query[0];
+      }
+      if (query[1]) {
+        queryParams.menu = query[1];
+      }
 
-    const actualPage = '/dashboard';
-    app.render(req, res, actualPage, queryParams);
- });
+      const actualPage = '/dashboard';
+      app.render(req, res, actualPage, queryParams);
+    });
 }
