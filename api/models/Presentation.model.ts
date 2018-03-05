ea2ebf27e3a7c66eef7a4c6288dbd2648e606232
Kohei Arai <tobecomebig@gmail.com>
2018-03-06 01:45:38 +0900

    Added validation to presentation save.

--- api/models/Presentation.model.ts
+++ api/models/Presentation.model.ts
@@ -1,4 +1,9 @@
-import { Model, model, Schema } from 'mongoose';
+import * as moment from 'moment-timezone';
+import { Model, model, Schema, Document } from 'mongoose';
+import { ObjectID } from 'bson';
+
+import DBUtil from '../utils/db.util';
+import Util from '../utils/util';
 
 const PresentationSchema = new Schema({
   start: {
@@ -47,7 +52,162 @@ PresentationSchema.pre('save', function (this: any, next) {
   }
   this.updated_at = new Date();
 
-  next();
+  presentationValidation(this, next);
 });
+
+/**
+ * Check if presentation time is valid.
+ * Valid presentation time is specified below:
+ * 1. start and end is one hour apart and start comes before end
+ * 2. Presentation start and end is in the range of presentationDate
+ * 3. All faculty is available at specifed time
+ * 4. No presentation exists which has the same group.adminFaculty and start and end overlaps 
+ *    The same SD 2 faculty cannot join two presentations at the same time
+ * 5. At least 30 minutes apart from all presentations if group.adminFaculty is different && share the one faculty in faculties
+ */
+const presentationValidation = (doc: Document, next: any) => {
+
+  // group is populated according to internal cache. 
+  // So if I stop populating group in DBUtil, this generates run time error.
+  let adminFaculty = doc.get('group').get('adminFaculty');
+  let semester = doc.get('semester');
+
+  // Check condition 1
+  new Promise((resolve, reject) => {
+    const start = moment(doc.get('start'));
+    const end = moment(doc.get('end'));
+
+    if (end.diff(start, 'hour') === 1) {
+      resolve();
+    } else {
+      reject(new Error('Presentation start time must be 1 hour and start must come before end'));
+    }
+  })
+    .then(() => {
+      return DBUtil.findPresentationDates({
+        semester,
+        admin: adminFaculty,
+      })
+    })
+    // Check condition 2
+    .then(presentationDates => {
+      if (presentationDates.length === 0) {
+        return Promise.reject(new Error('Presentation date is not defined for this admin faculty'));
+      } else {
+        const presentationDate = presentationDates[0];
+
+        const isInSemesterPresentationDate = presentationDate.get('dates')
+          .filter((date: Document) => Util.doesOverlap(date, doc))
+          .length > 0;
+
+        if (isInSemesterPresentationDate) {
+          return DBUtil.findAvailableSlots({
+            semester,
+            faculty: {
+              $in: doc.get('faculties')
+            }
+          }).exec();
+        } else {
+          return Promise.reject(new Error('Specified time is not in the semester presentation dates'));
+        }
+      }
+    })
+    // Check condition 3
+    .then((availableSlots: Document[]) => {
+      if (availableSlots.length !== doc.get('faculties').length) {
+        return Promise.reject(new Error('One of specified faculties do not exist'));
+      } else {
+        const isAllFacultiesAvailable = availableSlots
+          .map((availableSlot: Document) => {
+            return availableSlot.get('availableSlots')
+              .filter((availableSlot: Document) => Util.doesOverlap(availableSlot, doc))
+              .length > 0;
+          })
+          .length === doc.get('faculties').length;
+
+        if (isAllFacultiesAvailable) {
+          return DBUtil.findPresentations({
+            semester,
+          }).exec();
+        } else {
+          return Promise.reject(new Error('Some specified faculties is not available on specified presentation time range'));
+        }
+      }
+    })
+    // Check condition 4
+    .then((presentations: Document[]) => {
+      const overlappingGroups = presentations
+        .filter(presentation =>
+          presentation.get('_id').toString() !== doc.get('_id').toString() &&
+          presentation.get('group').get('adminFaculty') === adminFaculty
+        )
+        .map((presentation: Document) => {
+          // Map to -1 if presentation doesn't overlap with doc.
+          // Map to group number if presentation overlaps
+          if (Util.doesOverlap(doc, presentation)) {
+            return presentation.get('group').get('groupNumber');
+          } else {
+            return -1;
+          }
+        })
+        .filter((groupNumber: number) => groupNumber >= 0);
+
+      if (overlappingGroups.length > 0) {
+        return Promise.reject(new Error(`Presentation time conflicts with Group ${overlappingGroups.join(', ')}`))
+      } else {
+        return Promise.resolve(presentations);
+      }
+    })
+    // Check condition 5
+    .then((presentations: Document[]) => {
+      const facultiesIdArray = doc.get('faculties')
+        .map((objectId: ObjectID) => objectId.toString());
+
+      const facultiesInHurry = presentations
+        .filter(presentation =>
+          presentation.get('_id').toString() !== doc.get('_id').toString() &&
+          presentation.get('group').get('adminFaculty') !== adminFaculty &&
+          // Filter presentations that contain the duplicate faculties
+          presentation.get('faculties')
+            .map((objectId: ObjectID) => objectId.toString())
+            .filter((objectId: string) => facultiesIdArray.indexOf(objectId) >= 0)
+            .length > 0
+        )
+        .map((presentation: Document) => {
+          const d1StartM = moment(doc.get('start'));
+          const d1EndM = moment(doc.get('end'));
+          const d2StartM = moment(presentation.get('end'));
+          const d2EndM = moment(presentation.get('end'));
+
+          // d1 < d2
+          if (d1EndM.valueOf() < d2StartM.valueOf()) {
+            return d2StartM.diff(d1EndM, 'minutes', true) < 30;
+          }
+          // d2 < d1
+          else if (d2EndM.valueOf() < d1StartM.valueOf()) {
+            return d1StartM.diff(d2EndM, 'minutes', true) < 30;
+          }
+          // When two presentation ends and start at the same time, this condition is true
+          else {
+            return true;
+          }
+        })
+        .filter(facultiesInHurry => facultiesInHurry)
+        .length > 0;
+
+      if (facultiesInHurry) {
+        return Promise.reject(new Error('Faculties need at least 30 minutes apart if they move to different SD 2 faculty groups'));
+      } else {
+        return Promise.resolve();
+      }
+    })
+    .then(() => {
+      // All validation passes. Save it
+      next();
+    })
+    .catch((err: Error) => {
+      next(err);
+    });
+}
 
 export default model('Presentation', PresentationSchema);
--- api/utils/util.ts
+++ api/utils/util.ts
@@ -1,10 +1,11 @@
 import * as moment from 'moment-timezone';
+import { Document } from 'mongoose';
 
 export default class Util {
   static siteUrl() {
     return process.env.NODE_ENV === 'production' ? process.env.SITE_PRODUCTION_URL : process.env.SITE_DEVELOPMENT_URL;
   }
-  
+
   static currentSemesterKey() {
     return `${Util.currentYear()}_${Util.currentSeason()}`;
   }
@@ -22,4 +23,32 @@ export default class Util {
       return 'fall';
     }
   }
+
+  static doesOverlap(d1: Document, d2: Document) {
+    if (d1.get('_id') === d2.get('_id')) {
+      return false;
+    }
+
+    const start = d1.get('start').valueOf();
+    const end = d1.get('end').valueOf();
+    const otherStart = d2.get('start').valueOf();
+    const otherEnd = d2.get('end').valueOf();
+
+    // Logic is taken from https://github.com/rotaready/moment-range/blob/master/lib/moment-range.js#L185-L196
+    if ((start <= otherStart) && (otherStart < end) && (end < otherEnd)) {
+      return true;
+    }
+    else if ((otherStart < start) && (start < otherEnd) && (otherEnd <= end)) {
+      return true;
+    }
+    else if ((otherStart < start) && (start <= end) && (end < otherEnd)) {
+      return true;
+    }
+    else if ((start <= otherStart) && (otherStart <= otherEnd) && (otherEnd <= end)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
