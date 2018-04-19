41d5f9e176f741f6cd143a040a9b30620a56f77c
Kohei Arai <tobecomebig@gmail.com>
2018-04-18 18:46:18 -0700

    Updated top layout to use highlight instead of device photos

--- front/components/LP/top.tsx
+++ front/components/LP/top.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
-import { Row, Col, Button } from 'antd';
+import { Button } from 'antd';
 
 export interface AppProps {
 }
@@ -8,106 +8,77 @@ export interface AppProps {
 export default class App extends React.Component<AppProps, any> {
   render() {
     return (
-      <Row>
-        <Col
-          lg={{
-            span: 10,
-            offset: 2,
-          }}
-          xl={{
-            span: 9,
-            offset: 3,
-          }}
-        >
-          <div className="top">
-            <div className="title">SD Scheduler</div>
-            <div className="description">
-              Developed to make scheduling senior design presentation&nbsp;easier.
-            </div>
-            <div className="action">
+      <div className="top">
+        <h1 className="title">SD Scheduler</h1>
+        <div className="description">
+          Developed to make scheduling senior design presentation&nbsp;easier.
+        </div>
+        <div className="action">
+          <Button
+            type="primary"
+            size="large"
+            style={{ marginBottom: '16px' }}
+          >
+            Check committee member availability
+          </Button>
+          <span className="dummy-block"></span>
+          <Link href="/schedule">
+            <a>
               <Button
-                type="primary"
                 size="large"
                 style={{ marginBottom: '16px' }}
               >
-                Check committee member availability
+                Schedule presentation
               </Button>
-              <span className="dummy-block"></span>
-              <Link href="/schedule">
-                <a>
-                  <Button
-                    size="large"
-                    style={{ marginBottom: '16px' }}
-                  >
-                    Schedule presentation
-                  </Button>
-                </a>
-              </Link>
-            </div>
-          </div>
-        </Col>
-        <Col
-          md={{
-            span: 18,
-            offset: 3,
-          }}
-          lg={{
-            span: 10,
-            offset: 0,
-          }}
-          xl={{
-            span: 8,
-            offset: 0,
-          }}
-        >
-          <img src="/static/eye-catch-2.jpeg" alt="Mock image" style={{ width: '100%' }} />
-        </Col>
+            </a>
+          </Link>
+        </div>
         <style jsx>{`
+          .top {
+            background: #141E30;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #243B55, #141E30);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #243B55, #141E30); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            padding: 100px 0 85px 0;
+            text-align: center;
+          }
           .top .title {
-            margin-top: 100px;
             font-size: 68px;
             margin-bottom: 24px;
             line-height: 76px;
             font-weight: 600;
+            color: #fff;
           }
           .top .description {
             font-size: 20px;
             line-height: 40px;
             margin-bottom: 24px;
+            color: #fff;
           }
           .top .dummy-block {
             display: inline-block;
             width: 16px;
           }
-          @media (max-width: 991px) {
-            .top .title {
-              margin-top: 50px;
-            }
-            .top .title, .top .description, .top .action {
-              text-align: center;
-            }
-            .top .action {
-              margin-bottom: 16px;
-            }
-          }
-          @media (max-width: 513px) {
+          @media (max-width: 520px) {
             .top .dummy-block {
               display: block;
               width: 0;
             }
           }
-          @media (max-width: 415px) {
+          @media (max-width: 450px) {
             .top .title {
               font-size: 60px;
             }
           }
-          @media (max-width: 410px) {
+          @media (max-width: 400px) {
+            .top {
+              padding: 50px 0 35px 0;
+            }
             .top .title {
               font-size: 48px;
             }
           }
         `}</style>
-      </Row>
+      </div>
     );
   }
 }
