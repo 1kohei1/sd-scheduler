5028f3b7a5cc679c4d9393392998874f5a5ad3da
Kohei Arai <tobecomebig@gmail.com>
2018-04-19 18:42:04 -0700

    Added an image

--- front/components/LP/instruction.tsx
+++ front/components/LP/instruction.tsx
@@ -12,26 +12,55 @@ export default class Instruction extends React.Component<InstructionProps, any> 
           Schedule senior design presentation&nbsp;in&nbsp;5&nbsp;minutes.
         </div>
         <div className="box">
-          <Row>
-            <Col span={12}>
-              <div className="title">Check committee member availability.</div>
-            </Col>
-            <Col span={12}>
+          <div className="title">Check committee member availability.</div>
+          <Row style={{ marginBottom: '16px' }}>
+            <Col
+              xs={{
+                span: 16,
+                offset: 4,
+              }}
+              sm={{
+                span: 14,
+                offset: 5,
+              }}
+              span={12}
+              offset={6}
+            >
+              <img src="/static/prod-semester-calendar.png" style={{ width: '100%' }} />
             </Col>
           </Row>
-          <Row>
-            <Col span={12}>
-              <div className="title">Fill necessary information for your presentation.</div>
-            </Col>
-            <Col span={8}>
-              {/* <img src="/static/android-fill-presentation.jpeg" style={{ maxWidth: '500px', margin: 'auto' }} /> */}
+          <div className="title">Fill necessary information for your presentation.</div>
+          <Row style={{ marginBottom: '16px' }}>
+            <Col
+              xs={{
+                span: 16,
+                offset: 4,
+              }}
+              sm={{
+                span: 14,
+                offset: 5,
+              }}
+              span={12}
+              offset={6}
+            >
+              <img src="/static/prod-fill-information.png" style={{ width: '100%' }} />
             </Col>
           </Row>
-          <Row>
-            <Col span={12}>
-              <div className="title">Receive confirmation email. Group member, committee members, and sponsor members get confirmation email as well.</div>
-            </Col>
-            <Col span={12}>
+          <div className="title">Group member, committee members, and sponsor members get confirmation email when presentation is scheduled.</div>
+          <Row style={{ marginBottom: '16px' }}>
+            <Col
+              xs={{
+                span: 16,
+                offset: 4,
+              }}
+              sm={{
+                span: 14,
+                offset: 5,
+              }}
+              span={12}
+              offset={6}
+            >
+              <img src="/static/prod-email.png" style={{ width: '100%' }} />
             </Col>
           </Row>
         </div>
@@ -49,6 +78,7 @@ export default class Instruction extends React.Component<InstructionProps, any> 
           .instruction .title {
             font-size: 28px;
             font-weight: 300;
+            text-align: center;
           }
           @media (max-width: 727px) {
             .instruction .heading {
